aca0d36b414bac49b55b049f0c2dea5efee30e4e
Tyagirikant <97226845+Tyagirikant@users.noreply.github.com>
2022-01-13 19:11:52 +0530

    binding

---
+++ lib/binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+import 'controller/validation_controller.dart';
+
+class HomeBinding extends Bindings{
+
+  void dependencies(){
+    Get.lazyPut<HomeController>(() =>HomeController());
+  }
+}
